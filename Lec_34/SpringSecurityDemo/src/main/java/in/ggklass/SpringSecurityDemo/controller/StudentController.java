a3f8b5fe5f335bcd305b959ca45c73f3be345d2d
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2026-09-23 22:17:25 +0530

    Spring security intro and csrf token

---
+++ Lec_34/SpringSecurityDemo/src/main/java/in/ggklass/SpringSecurityDemo/controller/StudentController.java
@@ -0,0 +1,37 @@
+package in.ggklass.SpringSecurityDemo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+
+    @GetMapping
+    public ResponseEntity<String> getStudents() {
+        return ResponseEntity.ok("Student retrieved successfully");
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createStudent() {
+        return ResponseEntity.ok("Student created successfully");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteStudent() {
+        return ResponseEntity.ok("Student deleted successfully");
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateStudent() {
+        return ResponseEntity.ok("Student updated successfully");
+    }
+
+    @GetMapping("/csrf")
+    public CsrfToken getToken(CsrfToken csrfToken) {
+        return csrfToken;
+    }
+}
